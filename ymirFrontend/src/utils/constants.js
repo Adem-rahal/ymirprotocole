e962a2f5383c41b3adcfe21c42dfc67c42544c8d
argonaute <adem.rahal01@gmail.com>
2022-03-29 15:12:25 +0200

    website only

--- ymirFrontend/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-import abiToken from '../../artifacts/contracts/ERC1155HashToken.sol/ERC1155HashToken.json';
-import abiStaking from '../../artifacts/contracts/Staking.sol/StakingERC1155.json'
-
-
-
-export const stakingABI = abiStaking.abi;
-export const tokenABI = abiToken.abi;
-export const tokenContractAddress = '0x9D41e6302B478aeb401B1D3dcAb299F12E47316a';
-export const stakingContractAddress = '0xcFD2194988Eb5Fc1fb0FA61130F0d7358AB5f8FC';
